c275f995841185943e8aa9154b0809ba19d8217c
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-06-28 21:39:31 +0200

    Using .env for sensitive data

--- API.py
+++ API.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_cors import CORS
+
+import config
 
 from db import db
 
@@ -30,11 +32,41 @@ def categories_route():
     
         tmp_categories.append(tmp_cat)
 
-
-
     categories_dict = {
         "categories": tmp_categories
     }
 
     return categories_dict
+
+@app.route('/products')
+def products_route():
+    prod_instance = db('products')
+
+    tmp_products = []
+
+    rows = prod_instance.select()
+
+    for row in rows:
+        tmp_prod = {
+            "id": row[0],
+            "prod_name": row[1],
+            "prod_img": row[2], 
+            "prod_desc": row[3], 
+            "prod_slug": row[4], 
+            "prod_price": row[4]
+        }
+
+        tmp_products.append(tmp_prod)
+
+    products_dict = {
+        "products": tmp_products
+    }
+
+    return products_dict
+
+@app.route('/image/<img_name>')
+def get_image(img_name):
+    filename = f'images/{img_name}'
+
+    return send_file(filename, mimetype='image/jpg')
 
--- sql_statements.sql
+++ sql_statements.sql
@@ -18,6 +18,21 @@ VALUES ('Furniture', 'furniture'),
 ALTER TABLE categories
 ADD cat_img TEXT;
 
---updated cat_img column for all rows
+--Updated cat_img column for all rows
 UPDATE categories 
 SET cat_img= 'cat_img.jpg';
+
+--Create Products table
+CREATE TABLE products (
+	id SERIAL PRIMARY KEY,
+	prod_name TEXT NOT NULL UNIQUE,
+	prod_img TEXT NOT NULL UNIQUE,
+	prod_desc TEXT,
+	prod_slug TEXT NOT NULL UNIQUE,
+	prod_price DOUBLE PRECISION NOT NULL
+);
+
+INSERT INTO products (prod_name, prod_img, prod_desc, prod_slug, prod_price)
+VALUES ('Insta Mini 9', 'insta-mini-9.png', 'Selfie mode and selfie mirror, macro mode',
+	   'insta-mini-9', 99.85);
+
